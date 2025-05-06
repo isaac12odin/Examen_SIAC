5eb92db340fcee76d645c49cdc62c6b9bd2de884
isaac12odin <78758427+isaac12odin@users.noreply.github.com>
2025-05-05 20:52:48 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: false,
+  experimental: { appDir: false },
 };
 
 export default nextConfig;
